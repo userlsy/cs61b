bf898a19caf193c7db8cc201f9e54bc8673bd133
userlsy <3303323@gmail.com>
2023-04-16 12:00:16 +0800

    twice commit

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,5 +1,5 @@
 public class Planet {
-	static final double G = 6.67e-11;
+	private static final double G = 6.67e-11;
 
 	public double xxPos;  // Its current x position
 	public double yyPos;  // Its current y position
