d7610a34278cac028f7ce176753658970357e951
userlsy <3303323@gmail.com>
2023-08-29 20:52:06 +0800

    proj1b 4th commit

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -11,9 +11,11 @@ public class TestOffByOne {
     @Test
     public void testequalChars() {
         System.out.println(offByOne.equalChars('a', 'b'));  // true
+        assertTrue(offByOne.equalChars('a', 'b'));
         System.out.println(offByOne.equalChars('r', 'q'));  // true
 
         System.out.println(offByOne.equalChars('a', 'e'));  // false
+        assertFalse(offByOne.equalChars('a', 'e'));
         System.out.println(offByOne.equalChars('z', 'a'));  // false
         System.out.println(offByOne.equalChars('a', 'a'));  // false
 
@@ -21,7 +23,11 @@ public class TestOffByOne {
 
         System.out.println(offByOne.equalChars('a', 'B'));  // false
 
-        System.out.println(offByOne.equalChars('1', '2'));  // false
+        System.out.println(offByOne.equalChars('1', '2'));  //
+
+        System.out.println(offByOne.equalChars('1', '5'));
+        System.out.println(offByOne.equalChars('1', '%'));
+        System.out.println(offByOne.equalChars('@', '&'));
 
     }
     
